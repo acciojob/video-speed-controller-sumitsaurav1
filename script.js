fedc13723b2ba0a75b9e2a3407123ad1c3e5a416
Sumit saurav <117914094+sumitsaurav1@users.noreply.github.com>
2023-06-21 16:57:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,68 @@
 const inputs = document.querySelectorAll('.controls input');
-
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+/* Edit this file */
+/* Edit this file */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+// const ranges = player.querySelectorAll('.player__slider');
+console.log(skipButtons)
+function togglePlay(){
+    if(video.paused){
+        video.play()
+    }else{
+        video.pause()
     }
+    updateButton();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+// console.log(ranges)
+function updateButton(){
+    if(toggle.textContent==='►'){
+        toggle.textContent='❚❚'
+    }else{
+        toggle.textContent='►'
+    }
+}
+
+function skip(value){
+    let skipValue= parseFloat(value);
+    // console.log(skipValue)
+    video.currentTime += skipValue;
+}
+
+function volumeChange(){
+    video.volume= this.value
+}
+function playBackRateChange(){
+    video.playbackRate = this.value;
+}
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100;
+	progressBar.style.flexBasis = `${percent}%`;
+}
+function scrub(e){
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+	video.currentTime = scrubTime;
+}
+
+
+
+toggle.addEventListener('click',togglePlay);
+video.addEventListener('click',togglePlay);
+skipButtons.forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        let value = button.getAttribute('data-skip');
+        // console.log(value)
+        skip(value)
+    })
+})
+ranges[0].addEventListener('input',volumeChange)
+ranges[1].addEventListener('input',playBackRateChange)
+video.addEventListener('timeupdate', handleProgress);
+progress.addEventListener('click', scrub);
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
